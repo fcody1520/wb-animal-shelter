3049eb26c24ee6118cf9697be39b8e8284a9ac61
Cody Farrington <fcody1520@gmail.com>
2024-01-08 16:34:22 -0700

    on step 5

--- animal-shelter.js
+++ animal-shelter.js
@@ -1,1 +1,51 @@
 const animalData = require('./animal-data.json');
+
+class Animal {
+    constructor(name, species, color, hunger =50){
+        this.name = name
+        this.species = species
+        this.color = color
+        this. hunger = hunger
+    }
+
+    greet (){
+        console.log(`Hi, I'm ${this.name} the ${this.species}!`);
+    }
+
+    feed(){
+        this.hunger -= 20
+        console.log('Yum, I love food!');
+    }
+}
+
+class Cat extends Animal{
+    
+}
+
+
+class AnimalShelter{
+    constructor(){
+        this.animals = []
+    }
+
+    addAnimal(animal){
+        this.animals.push(animal)
+    }
+
+    adopt(animal){
+        const animalIndex = this.animals.indexOf(animal);
+        this.animals.splice(animalIndex,1)
+    }
+
+    getAnimalsBySpecies(species){
+        return this.animals.filter(a => a.species === species)
+    }
+}
+
+const shelter = new AnimalShelter();
+
+for (let a of animalData) {
+    const hunger = a.hunger ? a.hunger : 50;
+    const animal = new Animal(a.name, a.species, a.color, hunger)
+    Shelter.addAnimal(animal)
+}
